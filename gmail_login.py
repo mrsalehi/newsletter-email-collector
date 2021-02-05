8f67fae98e193c133c3121327f440528e5094b66
mrsalehi <salehi.mohammadreza1997@gmail.com>
2021-02-05 07:42:57 -0500

    Fetch/Suggestion/yaml_list working

--- gmail_login.py
+++ gmail_login.py
@@ -3,20 +3,18 @@ import imaplib
 import email
 from time import sleep
 import os
-from datetime import datetime, date
+from datetime import datetime
 import yaml
 from glob import glob
+import calendar
 
-
-today = date.today()
 
 GMAIL_USER = 'msreza76@gmail.com'
 GMAIL_PASSWORD = 'Salehi76primarypassword'
-
-mail = imaplib.IMAP4_SSL('imap.gmail.com', 993)
-mail.login(GMAIL_USER, GMAIL_PASSWORD)
-mail.list()
-mail.select('inbox')
+GMAIL = imaplib.IMAP4_SSL('imap.gmail.com', 993)
+GMAIL.login(GMAIL_USER, GMAIL_PASSWORD)
+GMAIL.list()
+GMAIL.select('inbox')
 
 os.makedirs('mails/The Batch', exist_ok=True)
 os.makedirs('mails/TechCrunch Week in Review', exist_ok=True)
@@ -30,7 +28,7 @@ NEWSLETTER_IDENTIFIERS = [  # Tuples of identifier key, value of identifier
         'Subject-prefix': 'Week in Review', 
         'Dest-folder': 'TechCrunch Week in Review'}, 
     {
-        'From': 'DeepLearning.AI <thebatch@deeplearning.ai>', 
+        'From': '"DeepLearning.AI" <thebatch@deeplearning.ai>', 
         'Subject-prefix': 'The Batch', 
         'Dest-folder': 'The Batch'},
     {
@@ -43,34 +41,105 @@ NEWSLETTER_IDENTIFIERS = [  # Tuples of identifier key, value of identifier
     ]
 
 
-while True:
-    retcode, mail_ids = mail.search(None, '(UNSEEN)')
+def get_yaml_list():
+    if os.path.exists('mail_list.yaml'):
+        with open('mail_list.yaml') as fptr:
+            return yaml.load(fptr, Loader=yaml.FullLoader)
     
+    yaml_list = {
+        'The Batch': [], 
+        'TechCrunch Startups Weekly': [], 
+        'TechCrunch Week in Review': [],
+        'The Download MIT': []}
+    
+    save_yaml_list(yaml_list)
+
+    return yaml_list
+
+
+def save_yaml_list(yaml_list):
+    with open('mail_list.yaml', 'w') as fptr:
+        yaml.dump(yaml_list, fptr)
+    
+
+def mark_as_seen_yaml_list(yaml_list, publisher, id_):
+    for el in yaml_list[publisher]:
+        if el['id'] == id_:
+            el[read] = True
+            return
+    
+    save_yaml_list(yaml_list)
+
+
+def add_to_yaml_list(yaml_list, publisher, date, subject, read=False):
+    id_ = len(yaml_list[publisher]) + 1 
+
+    yaml_list[publisher].append({
+        'id': id_,
+        'Date': date,
+        'Subject': subject,
+        'read': read
+    })
+
+    save_yaml_list(yaml_list)
+
+
+def check_for_new_mails():
+    retcode, mail_ids = GMAIL.search(None, '(UNSEEN)')
+    yaml_list = get_yaml_list()
+
+    mails = []
+
     for num in mail_ids[0].split():
-        typ, data = mail.fetch(num,'(RFC822)')
-        
-        for response_part in data:
-            if isinstance(response_part, tuple):
-                original = email.message_from_string(str(response_part[1], 'utf-8'))
+        ret, mail = GMAIL.fetch(num,'(RFC822)')
+
+        if ret == 'OK':
+            data = mail[0]
+            if isinstance(data, tuple):
+                mails.append(email.message_from_string(str(data[1], 'utf-8')))
+
+    return mails
+
+
+def suggest_reading(yaml_list):
+    for publisher in yaml_list:
+        for email in yaml_list[publisher]:
+            if not email['read']:
+                email['read'] = True
+                save_yaml_list(yaml_list)
+                return publisher, email
+
+    return None
+
+
+while True:
+    yaml_list = get_yaml_list()
+    mails = check_for_new_mails()
+
+    for mail in mails:
+        subject = mail['Subject']
+        print(mail['From'])
+        for identifier in NEWSLETTER_IDENTIFIERS:
+            if identifier['From'] == mail['From']:
+                prefix = identifier.get('Subject-prefix', None)
+                if prefix and subject.startswith(prefix):
+                    with open(os.path.join('mails', identifier['Dest-folder'], subject + '.eml'), 'w') as fp:
+                        fp.write(str(mail))
+                        add_to_yaml_list(
+                            yaml_list, 
+                            identifier['Dest-folder'],
+                            f'{datetime.today().day} {calendar.month_name[datetime.today().month]} {datetime.today().year}',
+                            subject)
                 
-                subject = original['Subject']
-
-                for identifier in NEWSLETTER_IDENTIFIERS:
-                    if identifier['From'] == original['From']:
-                        prefix = identifier.get('Subject-prefix', None)
-                        if prefix and subject.startswith(prefix):
-                            
-                            with open(os.path.join('mails', identifier['Dest-folder'], subject + '.eml'), 'w') as fp:
-                                fp.write(str(original))
-                        elif not prefix:
-                            with open(os.path.join('mails', identifier['Dest-folder'], subject + '.eml'), 'w') as fp:
-                                fp.write(str(original))
-
-                subject = original['Subject']
-
-
-                name = subject.replace(' ', '\ ') + '.eml'
-                path = f'mails/{name}'
-
-    break
-    sleep(60)  # Sleep for a little less than 30 minutes
+                elif not prefix:
+                    with open(os.path.join('mails', identifier['Dest-folder'], subject + '.eml'), 'w') as fp:
+                        fp.write(str(mail))
+                        add_to_yaml_list(
+                            yaml_list, 
+                            identifier['Dest-folder'],
+                            f'{datetime.today().day} {calendar.month_name[datetime.today().month]} {datetime.today().year}',
+                            subject)
+    
+    publisher, email = suggest_reading(yaml_list)
+    path = os.path.join('mails', publisher.replace(' ', '\ '), email['Subject'].replace(' ', '\ ') + '.eml')
+    os.system("open " + path)
